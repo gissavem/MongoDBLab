eefbb129b3360aeae8f14aa088d9b78b15da8bfa
John Andersson <john.andersson1995@gmail.com>
2020-01-16 13:38:40 +0100

    initialize project in VS

---
+++ MongoLab/MongoLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MongoLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
