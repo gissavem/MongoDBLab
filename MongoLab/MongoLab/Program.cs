d503eb9803c331aec56d34bf4d9a262cefce3896
Daniel <daniel.andersson@iths.se>
2020-01-17 13:23:02 +0100

    main

--- MongoLab/MongoLab/Program.cs
+++ MongoLab/MongoLab/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 
 namespace MongoLab
 {
@@ -6,7 +7,19 @@ namespace MongoLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //connection
+            MongoUrlBuilder mongoUrlBuilder = new MongoUrlBuilder();
+            mongoUrlBuilder.Scheme = MongoDB.Driver.Core.Configuration.ConnectionStringScheme.MongoDB;
+            mongoUrlBuilder.Server = new MongoServerAddress("localhost", 27017);
+            var clientSettings = mongoUrlBuilder.ToMongoUrl();
+            MongoClient mongoClient = new MongoClient(clientSettings);
+
+            var db = mongoClient.GetDatabase("Lab3");
+            string content = "";
+            var collection = db.GetCollection<Resturants>("resturants");
+            collection.InsertMany()
+
+
         }
     }
 }
