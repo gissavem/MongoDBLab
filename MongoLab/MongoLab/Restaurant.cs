a6dfbb25d6c30e37792adb18db144d437b7b000e
John Andersson <john.andersson1995@gmail.com>
2020-01-17 13:22:14 +0100

    finish restaurant class

--- MongoLab/MongoLab/Restaurant.cs
+++ MongoLab/MongoLab/Restaurant.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Text;
 using MongoDB.Driver;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace MongoLab
 {
     class Restaurant
     {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("stars")]
+        public int Stars { get; set; }
+        [BsonElement("categories")]
+        public List<string> Categories { get; set; }
+
     }
 }
